b7b156dd58e0ebaf6774c4169fb8e651b526dc91
Gabriel Landi <gabriellandi77@gmail.com>
2026-09-14 10:23:54 -0300

    Add files via upload

--- aula3/src/model/Projeto.java
+++ aula3/src/model/Projeto.java
@@ -1,1 +1,16 @@
+Projeto projeto1 = new Projeto(
+    1,
+    "Sistema Acadêmico",
+    "Sistema para gerenciamento acadêmico",
+    "Software",
+    "Em desenvolvimento"
+);
+
+Projeto projeto2 = new Projeto(
+    2,
+    "Site Institucional",
+    "Website institucional",
+    "Web",
+    "Concluído"
+);
 
